2e2fd511ed6c8c734f58029625d1fd7c3d4f5f33
rafglo <276020@student.pwr.edu.pl>
2024-03-23 17:29:21 +0100

    l1 zad 3

--- test.py
+++ test.py
@@ -1,24 +1,3 @@
-def conversion(x, b):
-    if x == 0:
-        return "0"
-    else:
-        result = ""
-        while x > 0:
-            rem = x % b
-            x //= b
-            result += str(rem)
-        return result[::-1]
-    
-def reversing(x):
-    x_rev = x[::-1]
-    return "0." + x_rev
-
-a = reversing(conversion(13,2))
-
-def reconversion(x, b):
-    result = 0
-    for i in range(2, len(x)):
-        result += int(x[i]) * b**(-(int(i)-1))
-    return result
-
-print(reconversion(a,2))
+import numpy as np
+a=[2,4,6]
+print(np.cumsum(a))
