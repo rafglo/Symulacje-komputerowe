34c2a361f3567a1834692ce2f9dbade7d90fb285
rafglo <276020@student.pwr.edu.pl>
2024-05-27 03:25:29 +0200

    e

--- test.py
+++ test.py
@@ -1,44 +1,51 @@
+import random
 import numpy as np
+import math
+from scipy.special import erfinv
 import matplotlib.pyplot as plt
-from scipy.stats import poisson
+from scipy.stats import norm
+import seaborn as sns
+n = 255
+v = 0.00492867323399
+r = 3.6541528853610088 
+def ziggurat2(n, r, v, mi, sigma):
+    xs = np.zeros(n+1)
+    xs[n] = r
+    for i in range(n-1,0,-1):
+        if -np.log(2*np.pi*(v/xs[i+1]+norm.pdf(xs[i+1], 0, 1))**2) < 0:
+            print(i)
+        xs[i] = np.sqrt(-np.log((v/xs[i+1]+norm.pdf(xs[i+1], 0, 1))**2))
+    run=True
+    while run:
+        u = np.random.rand(5)
+        i = 1 + math.floor(n * u[0])
+        x = xs[i] * u[1]
+        if np.absolute(x) < xs[i-1]:
+            X = x
+            run = False
+        if i != n:
+            y = (norm.pdf(xs[i-1]) - norm.pdf(xs[i])) * u[2]
+            if y < norm.pdf(x) - norm.pdf(xs[i]):
+                X = x
+                run = False
+        if np.absolute(x) >= xs[i-1]:
+            run2 = True
+            while run2:
+                d, f = -1 + 2*u[3], u[4]
+                x, y = -np.log(np.absolute(d))/3, -np.log(f)
+                if 2*y > x**2:
+                    if d > 0:
+                        X = x + 3
+                    else:
+                        X = -x - 3
+                    run = False
+    Y = sigma * X + mi
+    return Y
 
-def generate_poisson_process(lambda_, T, num_trajectories):
-    # Inicjalizacja listy przechowującej liczby zdarzeń N(t) dla każdej trajektorii
-    Nt_values = []
-
-    for _ in range(num_trajectories):
-        # Generowanie czasów między kolejnymi zdarzeniami
-        interarrival_times = np.random.exponential(scale=1/lambda_, size=int(lambda_*T*2))
-        # Sumowanie czasów oczekiwania
-        times = np.cumsum(interarrival_times)
-        # Filtrowanie czasów, aby pozostały tylko te mniejsze niż T
-        times = times[times < T]
-        # Obliczanie N(t)
-        Nt = len(times)
-        # Dodanie N(t) do listy
-        Nt_values.append(Nt)
-
-    return Nt_values
-
-# Parametry
-lambda_ = 0.5  # Intensywność procesu Poissona
-T = 10  # Długość odcinka czasu
-num_trajectories = 1000  # Liczba trajektorii
-
-# Generowanie trajektorii procesu Poissona
-Nt_values = generate_poisson_process(lambda_, T, num_trajectories)
-
-# Oczekiwana wartość N(t)
-expected_lambda_t = lambda_ * T
-
-# Sporządzenie histogramu liczby zdarzeń N(t)
-plt.hist(Nt_values, bins=range(max(Nt_values) + 2), density=True, alpha=0.5, label='Empiryczny')
-# Wygenerowanie rozkładu Poissona o oczekiwanej wartości lambda*t
-x = np.arange(0, max(Nt_values) + 1)
-poisson_distribution = poisson.pmf(x, mu=expected_lambda_t)
-plt.plot(x, poisson_distribution, 'ro-', label='Teoretyczny (Poisson)')
-plt.xlabel('Liczba zdarzeń (N(t))')
-plt.ylabel('Prawdopodobieństwo')
-plt.title('Porównanie empirycznego rozkładu liczby zdarzeń N(t) z rozkładem Poissona')
-plt.legend()
+samples = [ziggurat2(n,r,v,0,1) for i in range(100)]
+xs = np.linspace(-5,5, 100)
+plt.hist(samples, density=True, bins=10)
+plt.plot(xs, norm.pdf(xs, 0, 1), label="gęstość teoretyczna")
+plt.title("Porównanie unormowanego histogramu z gęstością rozkładu standardowego normalnego")
+plt.legend(loc='best')
 plt.show()
