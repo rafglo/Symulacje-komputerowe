00d1641c04e7ba51a69cc3cc72eb0812ed3f4ce0
rafglo <276020@student.pwr.edu.pl>
2024-05-28 01:28:37 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,51 +1,37 @@
-import random
 import numpy as np
-import math
-from scipy.special import erfinv
 import matplotlib.pyplot as plt
-from scipy.stats import norm
-import seaborn as sns
-n = 255
-v = 0.00492867323399
-r = 3.6541528853610088 
-def ziggurat2(n, r, v, mi, sigma):
-    xs = np.zeros(n+1)
-    xs[n] = r
-    for i in range(n-1,0,-1):
-        if -np.log(2*np.pi*(v/xs[i+1]+norm.pdf(xs[i+1], 0, 1))**2) < 0:
-            print(i)
-        xs[i] = np.sqrt(-np.log((v/xs[i+1]+norm.pdf(xs[i+1], 0, 1))**2))
-    run=True
-    while run:
-        u = np.random.rand(5)
-        i = 1 + math.floor(n * u[0])
-        x = xs[i] * u[1]
-        if np.absolute(x) < xs[i-1]:
-            X = x
-            run = False
-        if i != n:
-            y = (norm.pdf(xs[i-1]) - norm.pdf(xs[i])) * u[2]
-            if y < norm.pdf(x) - norm.pdf(xs[i]):
-                X = x
-                run = False
-        if np.absolute(x) >= xs[i-1]:
-            run2 = True
-            while run2:
-                d, f = -1 + 2*u[3], u[4]
-                x, y = -np.log(np.absolute(d))/3, -np.log(f)
-                if 2*y > x**2:
-                    if d > 0:
-                        X = x + 3
-                    else:
-                        X = -x - 3
-                    run = False
-    Y = sigma * X + mi
-    return Y
+import scipy.stats as stats
 
-samples = [ziggurat2(n,r,v,0,1) for i in range(100)]
-xs = np.linspace(-5,5, 100)
-plt.hist(samples, density=True, bins=10)
-plt.plot(xs, norm.pdf(xs, 0, 1), label="gęstość teoretyczna")
-plt.title("Porównanie unormowanego histogramu z gęstością rozkładu standardowego normalnego")
-plt.legend(loc='best')
+def iloraz(n, mi, sigma):
+    ys = np.zeros(n)
+    i = 0
+    while i < n:
+        u1, u2 = np.random.rand(2)
+        u, e = u1, np.exp(1)
+        v = -np.sqrt(2/e) + 2*np.sqrt(2/e) * u2
+        x = v / u
+        if x**2 <= 2 * (3 - u * (4 - u)):
+            ys[i] = sigma * x + mi
+            i += 1
+        elif (x**2 <= 2/u - 2*u) and (x**2 >= -4 * np.log(u)):
+            ys[i] = sigma * x + mi
+            i += 1
+    return ys
+
+# Generowanie próbek
+n = 100000
+mi = 0
+sigma = 1
+samples = iloraz(n, mi, sigma)
+
+# Histogram i wykres gęstości
+plt.hist(samples, bins=50, density=True, alpha=0.6, color='g')
+
+# Teoretyczna krzywa rozkładu normalnego
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = stats.norm.pdf(x, mi, sigma)
+plt.plot(x, p, 'k', linewidth=2)
+title = "Histogram and Normal Distribution Curve"
+plt.title(title)
 plt.show()
